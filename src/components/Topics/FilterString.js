a22b813dade10a9a7b775dbb3b151638af346f92
= <jthecybertinkerer@gmail.com>
2020-03-09 16:39:43 -0500

    FilterString: Layout Skeleton JSX

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 export default class FilterString extends Component {
   render() {
-    return <div>hello from class FilterString</div>;
+    return (
+      <div id="FilterString" className="puzzleBox filterStringPB">
+        <h4>Filter String</h4>
+        <span className="puzzleText"></span>
+        <input className="inputLine" value={""} onChange={() => {}} />
+        <button className="confirmationButton" onClick={() => {}}>
+          Filter the string
+        </button>
+        <span className="resultsBox filterStringRB"></span>
+      </div>
+    );
   }
 }
