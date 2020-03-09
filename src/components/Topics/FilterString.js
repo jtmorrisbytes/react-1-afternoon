be3e4bfe7a25b3fd0f5d50a94f8126c4a3bf1905
= <jthecybertinkerer@gmail.com>
2020-03-09 16:44:26 -0500

    FilterString: Set up initial state

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -10,7 +10,9 @@ export default class FilterString extends Component {
         "Chewing Gum",
         "Video Games",
         "long random list of random random words"
-      ]
+      ],
+      unfilteredArray: [],
+      userInput: ""
     };
   }
 
