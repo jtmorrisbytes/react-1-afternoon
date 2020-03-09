6c2f0752e4c3b327e480de91aacf1f077a60c062
= <jthecybertinkerer@gmail.com>
2020-03-09 16:00:35 -0500

    FilterObject: Layout JSX Skeleton

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 export default class FilterObject extends Component {
   render() {
-    return <div>hello from class FilterObject</div>;
+    return (
+      <div id="FilterObject" className="puzzleBox filterObjectPB">
+        <h4>FilterObject</h4>
+        <input className="inputLine" />
+        <span className="resultsBox filterObjectRB"></span>
+      </div>
+    );
   }
 }
