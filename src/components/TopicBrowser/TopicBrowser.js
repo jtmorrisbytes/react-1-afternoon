fb78db7cad0ca6c3943e9475a5b57a39aac47306
= <jthecybertinkerer@gmail.com>
2020-03-09 14:48:57 -0500

    Updated TopicBrowser with imports from the relative Topics files

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-import EvenAndOdd from "../Topics/EvenAndOdd/";
+import EvenAndOdd from "../Topics/EvenAndOdd";
+import FilterObject from "../Topics/FilterObject";
+import FilterString from "../Topics/FilterString";
+import Palindrome from "../Topics/Palindrome";
+import Sum from "../Topics/Sum";
 
 class TopicBrowser extends React.Component {
   constructor() {
@@ -9,7 +13,11 @@ class TopicBrowser extends React.Component {
   render() {
     return (
       <div id="TopicBrowser">
-        <p>Hello World</p>
+        <EvenAndOdd />
+        <FilterObject />
+        <FilterString />
+        <Palindrome />
+        <Sum />
       </div>
     );
   }
