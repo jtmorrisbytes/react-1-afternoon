8af7e6bd846216a8dfc71d751bb29cbc3233db23
= <jthecybertinkerer@gmail.com>
2020-03-09 16:13:35 -0500

    FilterObject: finished setup, ready to solve toy problem

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -8,15 +8,29 @@ export default class FilterObject extends Component {
           name: "Jordan",
           favFood: "Pizza"
         },
-        { name: "Brittany", favFood: "Chicken" }
-      ]
+        { name: "Brittany", hometown: "caddo mills" }
+      ],
+      filteredArray: [],
+      userInput: ""
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  handleInputChange(event) {
+    this.setState({ ...this.state, userInput: event.target.value });
+  }
+  filterArray() {
+    this.state.unFilteredArray.map(obj => {});
   }
   render() {
     return (
       <div id="FilterObject" className="puzzleBox filterObjectPB">
         <h4>FilterObject</h4>
-        <input className="inputLine" />
+        <input
+          className="inputLine"
+          onChange={this.handleInputChange}
+          value={this.state.userInput}
+        />
+        <button className="confirmationButton">Filter</button>
         <span className="resultsBox filterObjectRB"></span>
       </div>
     );
