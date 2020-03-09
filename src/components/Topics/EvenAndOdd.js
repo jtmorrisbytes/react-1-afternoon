cde4f7ffe91566d8630b5d517a14dd7d0e02ee1a
= <jthecybertinkerer@gmail.com>
2020-03-09 15:55:45 -0500

    EvenAndOdd: solved toy problem. ready to move to next component

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -8,6 +8,7 @@ export default class EvenAndOdd extends Component {
       userInput: ""
     };
     this.splitArrayRegex = /(\d+),?/g;
+    this.isValidListRegex = /(\d+),?/;
     // function.call(this) might be a better solution for
     // one off fires.
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -16,11 +17,34 @@ export default class EvenAndOdd extends Component {
   handleInputChange(event) {
     this.setState({ ...this.state, userInput: event.target.value });
   }
-  handleListSubmit() {}
-  isListInputValid(inputString) {
-    return this.splitArrayRegex.test(inputString);
+  handleListSubmit() {
+    if (this.isListInputValid(this.state.userInput)) {
+      let numbers = this.state.userInput.split(",");
+      let evens = [];
+      let odds = [];
+      numbers.forEach(number => {
+        if (number % 2 === 0) {
+          evens.push(number);
+        } else {
+          odds.push(number);
+        }
+      });
+      this.setState({
+        ...this.state,
+        evensArray: evens,
+        oddsArray: odds
+      });
+    } else {
+      console.log("not a valid list", this.state.userInput);
+    }
   }
-  getNumbersFromList() {}
+  isListInputValid(inputString) {
+    if (inputString.endsWith(",")) {
+      return false;
+    }
+    return this.isValidListRegex.test(inputString);
+  }
+  calculateOddEven() {}
   render() {
     return (
       <div className="puzzleBox evenAndOddPB">
@@ -31,9 +55,11 @@ export default class EvenAndOdd extends Component {
           placeholder="1,2,3,4"
           className="inputLine"
         />
-        <button className="confirmationButton">Submit</button>
-        <span id="resultsBox">Evens</span>
-        <span id="resultsBox">Odds</span>
+        <button className="confirmationButton" onClick={this.handleListSubmit}>
+          Submit
+        </button>
+        <span className="resultsBox">{this.state.evensArray.join(", ")}</span>
+        <span className="resultsBox">{this.state.oddsArray.join(", ")}</span>
       </div>
     );
   }
