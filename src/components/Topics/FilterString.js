12ec94426fc2ba144b04ec9e48a1d1c82744b043
= <jthecybertinkerer@gmail.com>
2020-03-09 16:43:31 -0500

    FilterString: Added list of random words to filter

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 export default class FilterString extends Component {
+  constructor() {
+    super();
+    this.state = {
+      unFilteredArray: [
+        "Potatoes",
+        "meatballs",
+        "onions",
+        "Chewing Gum",
+        "Video Games",
+        "long random list of random random words"
+      ]
+    };
+  }
+
   render() {
     return (
       <div id="FilterString" className="puzzleBox filterStringPB">
         <h4>Filter String</h4>
-        <span className="puzzleText"></span>
+        <span className="puzzleText">
+          {this.state.unFilteredArray.join(", ")}
+        </span>
         <input className="inputLine" value={""} onChange={() => {}} />
         <button className="confirmationButton" onClick={() => {}}>
           Filter the string
