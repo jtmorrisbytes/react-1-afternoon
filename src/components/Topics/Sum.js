76d9f29ecee413c50d0605444ae9f63552e1a073
= <jthecybertinkerer@gmail.com>
2020-03-10 15:16:38 -0500

    Layout JSX Skeleton

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
 export default class ClassName extends Component {
   render() {
-    return <div>hello from class Sum</div>;
+    return (
+      <div id="Sum">
+        <input
+          className="inputLine"
+          value={this.state.number1}
+          onChange={this.handleNumber1Change}
+        />
+        <input
+          className="inputLine"
+          value={this.state.number2}
+          onChange={this.handleNumber2Change}
+        />
+        <button className="confirmationButton" onClick={this.calculateSum}>
+          Find Sum
+        </button>
+        <span className="resultsBox">
+          {this.state.sum
+            ? `${this.state.number1} + ${this.state.number2} = ${this.state.sum}`
+            : null}
+        </span>
+      </div>
+    );
   }
 }
