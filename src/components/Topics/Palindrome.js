eb4d9b0b56ccf42b48be08ca39a24827ceccc8f7
= <jthecybertinkerer@gmail.com>
2020-03-09 20:07:42 -0500

    Palindrome: scaffold

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 export default class Palindrome extends Component {
+  constructor() {
+    super();
+    super(i);
+  }
   render() {
-    return <div>hello from class Palindrome</div>;
+    return (
+      <div className="puzzleBox filterStringPB">
+        <h4>Palindrome</h4>
+        <input className="inputLine" />
+        <button className="confirmationButton">Check Palindrome</button>
+        <span className="resultsBox"></span>
+      </div>
+    );
   }
 }
